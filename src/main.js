dbd1055026b28104f6b4ab0589d25e8de77d1923
wumch <wumuch@gmail.com>
2018-06-02 04:19:27 +0800

    on the way

--- src/main.js
+++ src/main.js
@@ -1,26 +1,16 @@
 
+'use strict';
+
 const {app, BrowserWindow} = require('electron');
 const robot = require('robotjs');
 
+let win = null;
 const startup = function() {
-    const win = new BrowserWindow({width: 800, height: 600});
-    win.openDevTools();
-    win.webContents.loadFile('./main.html');
+    win = new BrowserWindow({width: 1200, height: 800});
     win.on('close', function () {
     });
-
+    win.loadURL('file://' + __dirname + '/main.html');
     robot.setMouseDelay(2);
-
-    let twoPI = Math.PI * 2.0;
-    let screenSize = robot.getScreenSize();
-    let height = (screenSize.height / 2) - 10;
-    let width = screenSize.width;
-
-    for (let x = 0; x < width; x++)
-    {
-        let y = height * Math.sin((twoPI * x) / width) + height;
-        robot.moveMouse(x, y);
-    }
 };
 
 app.on('ready', startup);
